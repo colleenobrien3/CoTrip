4378dda0191a92f823e0151e916204d38d8436f0
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-08 17:06:27 -0500

    Setup of previous trip card

---
+++ client/src/components/PrevTripCard/PrevTripCard.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import "./PrevTripCard.css";
+
+// Function based React Component
+const PrevTripCard = (props) => {
+
+  // Default Class to apply to Component
+  let classList = `PrevTripCard`;
+
+
+  return(
+    <div className={classList}>
+      PrevTripCard
+    </div>
+  );
+}
+
+export default PrevTripCard;
---
+++ client/src/components/PrevTripCard/PrevTripCard.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import PrevTripCard from './PrevTripCard';
+
+storiesOf('PrevTripCard', module)
+  .add("Default", () => (
+    <PrevTripCard />
+  )
+)
