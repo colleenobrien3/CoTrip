b23379c84e08a8b8cf3125fa955cd6b670b6a6a8
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-08 18:18:07 -0500

    skeleton of compoment is good

--- client/src/components/PrevTripCard/PrevTripCard.js
+++ client/src/components/PrevTripCard/PrevTripCard.js
@@ -1,19 +1,24 @@
-
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 import "./PrevTripCard.css";
+import "../../App.css";
 
 // Function based React Component
-const PrevTripCard = (props) => {
-
+const PrevTripCard = props => {
   // Default Class to apply to Component
   let classList = `PrevTripCard`;
 
-
-  return(
+  return (
     <div className={classList}>
-      PrevTripCard
+      <div className="prev-card-inner">
+        <div className="prev-card-title">Previous Trips</div>
+        <div className="prev-trip-images">
+          <div className="prev-trip-one">test</div>
+          <div className="prev-trip-two">test</div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default PrevTripCard;
