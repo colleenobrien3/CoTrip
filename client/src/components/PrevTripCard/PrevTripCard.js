5c0c8cf8115d9f80958d964b3c3d62c587e81f2c
William Chrapcynski <wchrapcynski@gmail.com>
2019-12-08 20:23:44 -0500

    Working further on adjusting positions

--- client/src/components/PrevTripCard/PrevTripCard.js
+++ client/src/components/PrevTripCard/PrevTripCard.js
@@ -13,10 +13,25 @@ const PrevTripCard = props => {
       <div className="prev-card-inner">
         <div className="prev-card-title">Previous Trips</div>
         <div className="prev-trip-images">
-          <div className="prev-trip-one">test</div>
-          <div className="prev-trip-two">test</div>
+          <div className="prev-trip-one">
+            <div className="prev-trip-location">
+              Location
+            </div>
+            <div className="prev-trip-year">
+              Year
+            </div>
+          </div>
+          <div className="prev-trip-two">
+            <div className="prev-trip-location">
+              Location
+            </div>
+            <div className="prev-trip-year">
+              Year
+            </div>
+          </div>
         </div>
       </div>
+      <div className="yellow-nav"></div>
     </div>
   );
 };
