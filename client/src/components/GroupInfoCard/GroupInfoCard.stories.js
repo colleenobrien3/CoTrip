7e62a35fff45b21a4de3868bf8a6f7f6aa6e877e
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-12-09 15:09:14 -0500

    separating out components

--- client/src/components/GroupInfoCard/GroupInfoCard.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import GroupInfoCard from './GroupInfoCard';
-
-storiesOf('GroupInfoCard', module)
-  .add("Default", () => (
-    <GroupInfoCard />
-  )
-)
--- client/src/components/LocationInfoCard/LocationInfoCard.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import LocationInfoCard from './LocationInfoCard';
-
-storiesOf('LocationInfoCard', module)
-  .add("Default", () => (
-    <LocationInfoCard />
-  )
-)
