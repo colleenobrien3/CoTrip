d158e862e8193081f2abe8fbc1d98e0464e90639
Conor Johnston <cnrswim@yahoo.com>
2019-12-16 15:24:20 -0500

    including atomic comment body while waiting for comment

--- client/src/components/PostCommentWhole/PostCommentWhole.js
+++ client/src/components/PostCommentWhole/PostCommentWhole.js
@@ -1,17 +1,16 @@
 
 import React from 'react';
 import ForumPost from '../ForumPost/ForumPost';
+import CommentBody from '../CommentBody/CommentBody';
 import "./PostCommentWhole.css";
 
 // Function based React Component
 const PostCommentWhole = (props) => {
 
-  // Default Class to apply to Component
-  let classList = `PostCommentWhole`;
-
   return(
     <div className='PostCommentWhole'>
       <ForumPost />
+      <CommentBody body="Hello" date="today" time="12 pm"/>
     </div>
   );
 }
