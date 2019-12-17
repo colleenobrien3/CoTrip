113b9568c428bc44697516574c88928199768042
DallasLind <mcgraw.dallas@gmail.com>
2019-12-17 09:15:51 -0500

    Updating location models but still need to figure out why its not showing up on psql

--- api/cotrip_app/models.py
+++ api/cotrip_app/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Location(models.Model):
-    city = models.CharField(max_length=100)
+    city = models.CharField(max_length=80)
     state = models.CharField(max_length=50)
     country = models.CharField(max_length=50)
+
 
     def __str__(self):
         return self.city + ", " + self.state
